6621c50c217893128be5a77c60d08266d8dbf605
John R <jonathan.r1997@gmail.com>
2022-03-01 00:27:12 +0000

    Login Fixxed again

--- AssignmentProject/Screens/LoginScreen.js
+++ AssignmentProject/Screens/LoginScreen.js
@@ -11,46 +11,51 @@ const storeData = async (key, value) => {
     // saving error
   }
 };
-function ValidateCredentials(pEmail, pPassword) {
-  return fetch(
-    'http://localhost:3333/api/1.0.0/login',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        email: pEmail,
-        password: pPassword,
-      }),
-    },
-  )
-    .then((response) => response.json())
-    .then((responseJson) => {
-      console.log("is this code even being executed")
-      if (responseJson.id !== null) {
-        console.log(responseJson);
-        storeData('id', responseJson.id);
-        storeData('token', responseJson.token);
-        console.log(responseJson.id);
-        console.log("This hsould be false")
-        setIsSignedIn(true)
-      } else {
-        //Throw error or inform user
-        console.log("Error signing in");
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-}
 
 const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [isSignedIn, setIsSignedIn] = useState()
+  const [isSignedIn, setIsSignedIn] = useState(false)
   const { signIn } = React.useContext(AuthContext);
 
+  const ValidateCredentials = (pEmail, pPassword) => {
+    return fetch(
+      'http://localhost:3333/api/1.0.0/login',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: pEmail,
+          password: pPassword,
+        }),
+      },
+    )
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log("is this code even being executed")
+        if (responseJson.id !== null) {
+          console.log(responseJson);
+          storeData('id', responseJson.id);
+          storeData('token', responseJson.token);
+          console.log(responseJson.id);
+          console.log("This hsould be false")
+          setIsSignedIn(true)
+        } else {
+          //Throw error or inform user
+          console.log("Error signing in");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+  
+  if (isSignedIn){
+    signIn({ email, password });
+  }
+  
   return (
     <View>
       <Text>Login Screen</Text>
@@ -60,7 +65,6 @@ const LoginScreen = ({ navigation }) => {
         title="Sign in"
         onPress={() => {
           ValidateCredentials(email, password);
-          signIn({ email, password });
         }}
       />
       <Button
